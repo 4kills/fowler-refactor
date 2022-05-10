4f2824ab335732d863840183b7e98006d7751827
BuildTools <dominik.ochs@gmx.net>
2022-05-10 11:40:51 +0200

    rename variable: thisamount -> amount

--- src/Customer.java
+++ src/Customer.java
@@ -42,23 +42,23 @@ class Customer {
     }
 
     private double amountFor(Rental each) {
-        double thisAmount = 0;
+        double amount = 0;
         switch (each.getMovie().getPriceCode()) {
             case REGULAR:
-                thisAmount += 2;
+                amount += 2;
                 if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    amount += (each.getDaysRented() - 2) * 1.5;
                 break;
             case NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                amount += each.getDaysRented() * 3;
                 break;
             case CHILDREN:
-                thisAmount += 1.5;
+                amount += 1.5;
                 if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    amount += (each.getDaysRented() - 3) * 1.5;
                 break;
         }
-        return thisAmount;
+        return amount;
     }
 
 }
