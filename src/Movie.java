057bb687d5ac20db122afa846cd3ac22f79da95e
BuildTools <dominik.ochs@gmx.net>
2022-05-10 12:24:49 +0200

    replace conditional with enum/functional programming: move calcuation of movie price to movie

--- src/Movie.java
+++ src/Movie.java
@@ -1,8 +1,31 @@
+import java.util.function.Function;
+
 public class Movie {
     public enum Code {
-        CHILDREN,
-        REGULAR,
-        NEW_RELEASE
+        CHILDREN(daysRented -> {
+            double amount = 1.5;
+            if (daysRented > 3)
+                amount += (daysRented - 3) * 1.5;
+            return amount;
+        }),
+        REGULAR(daysRented -> {
+            double amount = 2;
+            if (daysRented > 2)
+                amount += (daysRented - 2) * 1.5;
+            return amount;
+        }),
+        NEW_RELEASE(daysRented ->
+                daysRented * 3.0);
+
+        private final Function<Integer, Double> price;
+
+        public double getPrice(int daysRented) {
+            return price.apply(daysRented);
+        }
+
+        Code(Function<Integer, Double> price) {
+            this.price = price;
+        }
     }
 
     private final String title;
--- src/Rental.java
+++ src/Rental.java
@@ -13,22 +13,6 @@ class Rental {
     }
 
     public double price() {
-        double amount = 0;
-        switch (getMovie().getPriceCode()) {
-            case REGULAR:
-                amount += 2;
-                if (getDaysRented() > 2)
-                    amount += (getDaysRented() - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                amount += getDaysRented() * 3;
-                break;
-            case CHILDREN:
-                amount += 1.5;
-                if (getDaysRented() > 3)
-                    amount += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return amount;
+        return getMovie().getPriceCode().getPrice(daysRented);
     }
 }
