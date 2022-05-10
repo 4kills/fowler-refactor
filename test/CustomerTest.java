a97ca241eaa0846959dfb03823a349aa514a0a8e
BuildTools <dominik.ochs@gmx.net>
2022-05-10 09:49:31 +0200

    initial commit

---
+++ test/CustomerTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CustomerTest {
+
+    @Test
+    public void amountFor() {
+        Assertions.assertEquals(2, 2);
+    }
+}
