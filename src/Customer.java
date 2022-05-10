0925a702c7e22ba00d76140d8e452e270da726e0
BuildTools <dominik.ochs@gmx.net>
2022-05-10 12:00:05 +0200

    remove superflous comments

--- src/Customer.java
+++ src/Customer.java
@@ -21,13 +21,13 @@ class Customer {
         result.append("\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n");
 
         for (Rental each : rentals) {
-            //determine amounts for each line
             double price = each.price();
-            // add frequent renter points
             frequentRenterPoints ++;
+
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1)
                 frequentRenterPoints ++;
+            
             //show figures for this rental
             result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(price).append("\n");
             totalAmount += price;
